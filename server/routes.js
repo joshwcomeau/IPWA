70d65dd417eb2ca73c89ae2671dfc4c2dd73e26d
Joshua Comeau <joshwcomeau@gmail.com>
2016-05-01 08:48:44 -0400

    Add server route for processing uploads

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,12 @@
+import fs from 'fs';
 import path from 'path';
+import multer from 'multer';
 
 import { prepareGrid } from './utils/grid';
 import ws from './websocket-client';
 
+
+const upload = multer({ dest: 'uploads/' });
 
 export default function(app) {
   app.get('*', (req, res) => {
@@ -13,5 +17,11 @@ export default function(app) {
     const grid = prepareGrid(body.cells);
     ws.send(JSON.stringify(grid));
     res.json({ ok: true })
+  });
+
+  app.post('/process-upload', upload.single('image'), (req, res) => {
+    console.log("Received image", req.file)
+
+    res.json({ upload: true })
   });
 }
