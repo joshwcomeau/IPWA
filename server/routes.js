1b5e55c077524dfd008964285d92dca002e89f34
Joshua Comeau <joshwcomeau@gmail.com>
2016-05-12 09:47:37 -0400

    Refactor async/await route for /process-upload

--- server/routes.js
+++ server/routes.js
@@ -24,31 +24,56 @@ export default function(app) {
   });
 
   app.post('/process-upload', upload.single('image'), async (req, res) => {
-    let pixels;
-
-    // Convert callback-based modules to promises
     const readFilePromise = wrapWithPromise(fs.readFile);
     const writeFilePromise = wrapWithPromise(fs.writeFile);
-    const imageMagickConvertPromise = wrapWithPromise(imageMagick.convert);
+    const imConvertPromise = wrapWithPromise(imageMagick.convert);
 
+    // These helper methods aren't as necessary anymore, but they still
+    // provide semantic value that makes the route easier to follow.
+    const resizeAndConvert = buffer => (
+      imConvertPromise({
+        srcData: buffer,
+        width: 32,
+        height: 16,
+        format: 'PNG'
+      })
+    );
+
+    const readPixelsFromImage = buffer => (
+      imageMagick.getConstPixels({
+        srcData: buffer,
+        x: 0,
+        y: 0,
+        columns: 32,
+        rows: 16
+      })
+    );
+
+    // Our saveToDisk method is now much more intuitive; no hidden complexity!
+    const saveToDisk = (file, buffer) => (
+      writeFilePromise(getPathForNewFile(file), buffer)
+    );
+
+    // Because this is native javascript, native constructs like try/catch,
+    // `if` statements, and loops can be used!
     try {
+      // The `await` keyword instructs the engine to wait, in a non-blocking
+      // way, for the function to return before continuing.
       const originalFileBuffer = await readFilePromise(req.file.path);
-      const smallFileBuffer = await imageMagickConvertPromise({
-        srcData: originalFileBuffer, width: 32, height: 16, format: 'PNG'
-      });
+      const smallFileBuffer = await resizeAndConvert(originalFileBuffer);
 
-      pixels = readPixelsFromBuffer(smallFileBuffer)
+      await saveToDisk(req.file, smallFileBuffer);
 
-      await writeFilePromise(getPathForNewFile(req.file), smallFileBuffer);
+      const pixels = readPixelsFromImage(smallFileBuffer);
 
       // TEMP: Send to Pi as well
       const grid = prepareGridForPi(pixels);
       ws.send(JSON.stringify(grid));
 
+      return res.json({ done: true, pixels });
+
     } catch (err) {
       throw err;
     }
-
-    res.json({ done: true, pixels })
   });
 }
